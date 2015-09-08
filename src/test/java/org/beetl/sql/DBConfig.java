7464922ca69f15e1d55e8ae9220fb2c0b1f39b80
程序猿猴 <qdlxsxj@126.com>
2015-09-08 12:18:15 +0800

    sorry

--- src/test/java/org/beetl/sql/DBConfig.java
+++ src/test/java/org/beetl/sql/DBConfig.java
@@ -3,8 +3,8 @@ package org.beetl.sql;
 public class DBConfig {
 	public static String driver = "com.mysql.jdbc.Driver";
     public static String dbName = "test";
-    public static String password = "123456";
-    public static String userName = "sue";
-    public static String url = "jdbc:mysql://127.0.0.1:3306/" + dbName;
+    public static String password = "";
+    public static String userName = "root";
+    public static String url = "jdbc:mysql://127.0.0.1:3307/" + dbName;
 
 }
