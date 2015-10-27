b8aa39dcf62d9437df5f63ee0426012c1ec76094
Gavin <hiredme@163.com>
2015-10-27 20:03:17 +0800

    ===号以下，所有  *  号打头并连续的行 为注释行

--- src/main/java/org/beetl/sql/core/ClasspathLoader.java
+++ src/main/java/org/beetl/sql/core/ClasspathLoader.java
@@ -148,9 +148,7 @@ public class ClasspathLoader implements SQLLoader {
 			while ((temp = bf.readLine()) != null) {
 				temp = temp.trim();
 				lineNum++;
-				if (temp.startsWith("*")) {//读到注释行，不做任何处理
-					continue;
-				}else if (temp.startsWith("===")) {// 读取到===号，说明上一行是key，下面是SQL语句
+				if (temp.startsWith("===")) {// 读取到===号，说明上一行是key，下面是注释或者SQL语句
 					if (!list.isEmpty() && list.size() > 1) {// 如果链表里面有多个，说明是上一句的sql+下一句的key
 						String tempKey = list.pollLast();// 取出下一句sql的key先存着
 						sql = new StringBuilder();
@@ -163,6 +161,17 @@ public class ClasspathLoader implements SQLLoader {
 						sqlSourceMap.put(modelName + key, source);// 放入map
 						list.addLast(tempKey);// 把下一句的key又放进来
 						findLineNum = lineNum;
+					}
+					String tempNext = null;
+					while((tempNext = bf.readLine()) != null){//处理注释的情况
+						tempNext = tempNext.trim();
+						lineNum++;
+						if (tempNext.startsWith("*")) {//读到注释行，不做任何处理
+							continue;
+						}else{
+							list.addLast(temp);//===下面不是*号的情况，是一条sql
+							break;//读到一句sql就跳出循环
+						}
 					}
 				} else {
 					list.addLast(temp);
