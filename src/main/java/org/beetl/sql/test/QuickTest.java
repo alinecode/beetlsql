3cb8c5e8674b1ea005f7b0f5ee79db73a25c90d7
xiandafu <xiandafu@126.com>
2016-02-22 22:08:43 +0800

    2.0.2

--- src/main/java/org/beetl/sql/test/QuickTest.java
+++ src/main/java/org/beetl/sql/test/QuickTest.java
@@ -21,8 +21,10 @@ public class QuickTest {
 		SQLLoader loader = new ClasspathLoader("/org/beetl/sql/test");
 
 		SQLManager 	sql = new SQLManager(style,loader,cs,new DefaultNameConversion(), new Interceptor[]{new DebugInterceptor()});
-		List<String> list = sql.execute("select name from user where 1!=1", String.class, null);
+		List<String> list = sql.execute("select name from user where 1=1", String.class, null);
 		System.out.println(list);
+//		User user  = sql.unique(User.class, 1555);
+//		System.out.println("kk"+user.getName());
 	}
 
 }
