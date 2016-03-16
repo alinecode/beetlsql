adb4b9b7481f34f4615bcac5bc670698d3550bbe
woate <woate@qq.com>
2016-03-16 23:34:53 +0800

    增加单元测试

--- src/test/java/org/beetl/sql/ext/spring4/dao2/Demo2Mapper.java
+++ src/test/java/org/beetl/sql/ext/spring4/dao2/Demo2Mapper.java
@@ -1,10 +1,11 @@
 package org.beetl.sql.ext.spring4.dao2;
 
 import org.beetl.sql.core.mapper.BaseMapper;
+import org.beetl.sql.ext.spring4.UserEntity;
 import org.beetl.sql.test.User;
 
 /**
  * 定义一个演示用的Mapper
  */
-public interface Demo2Mapper extends BaseMapper<User>{
+public interface Demo2Mapper extends BaseMapper<UserEntity>{
 }
