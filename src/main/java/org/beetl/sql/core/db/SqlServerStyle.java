a10e455e0e72fd87993d5a062b29c35821f22e0b
xiandafu <xiandafu@126.com>
2016-09-11 19:39:52 +0800

    a

--- src/main/java/org/beetl/sql/core/db/SqlServerStyle.java
+++ src/main/java/org/beetl/sql/core/db/SqlServerStyle.java
@@ -1,12 +1,6 @@
 package org.beetl.sql.core.db;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 import java.util.Map;
-
-import org.beetl.sql.core.annotatoin.AssignID;
-import org.beetl.sql.core.annotatoin.AutoID;
-import org.beetl.sql.core.annotatoin.SeqID;
 
 public class SqlServerStyle extends AbstractDBStyle {
 
@@ -21,19 +15,15 @@ public class SqlServerStyle extends AbstractDBStyle {
 				+" ) inner_query ) select * from query where beetl_rn between "+HOLDER_START+OFFSET+HOLDER_END+" and "+HOLDER_START+PAGE_END+HOLDER_END;
 	}
 
-//	@Override
-//	public void initPagePara(Map<String, Object> paras,long start,long size) {
-//		paras.put(DBStyle.OFFSET,(start-1)*size+1);//开始索引   (pageNumber-1)*pageSize+1
-//		paras.put(DBStyle.PAGE_END,start*size);//结束索引 pageNumber*pageSize
-//	}
 
 	
 	@Override
 	public void initPagePara(Map<String, Object> paras,long start,long size) {
 		long s = start+(this.offsetStartZero?1:0);
 		paras.put(DBStyle.OFFSET,s);
-		paras.put(DBStyle.PAGE_END,size);
+		paras.put(DBStyle.PAGE_END,s+size-1);
 	}
+
 
 
 	@Override
