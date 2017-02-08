67017ed1045a9fb1563027af98b60bb4d6a9c118
李家智 <lijiazhi@coamc.com>
2017-02-08 17:49:27 +0800

    bug fix

--- src/main/java/org/beetl/sql/core/mapping/type/DateTypeHandler.java
+++ src/main/java/org/beetl/sql/core/mapping/type/DateTypeHandler.java
@@ -1,14 +1,13 @@
 package org.beetl.sql.core.mapping.type;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class DateTypeHandler extends JavaSqlTypeHandler {
 
 	@Override
 	public Object getValue(TypeParameter typePara) throws SQLException{
-		java.sql.Date a = typePara.rs.getDate(typePara.index);
+		Timestamp a = typePara.rs.getTimestamp(typePara.index);
 		if(a!=null){
 			return new java.util.Date(a.getTime());
 		}else{
--- src/main/java/org/beetl/sql/test/QuickTest.java
+++ src/main/java/org/beetl/sql/test/QuickTest.java
@@ -1,11 +1,13 @@
 package org.beetl.sql.test;
 
+import java.util.List;
+
 import org.beetl.sql.core.ClasspathLoader;
 import org.beetl.sql.core.Interceptor;
 import org.beetl.sql.core.SQLLoader;
 import org.beetl.sql.core.SQLManager;
 import org.beetl.sql.core.UnderlinedNameConversion;
-import org.beetl.sql.core.db.MySqlStyle;
+import org.beetl.sql.core.db.OracleStyle;
 import org.beetl.sql.ext.DebugInterceptor;
 
 /**
@@ -18,17 +20,16 @@ public class QuickTest {
 	
 	public static void main(String[] args) throws Exception{
 
-		MySqlStyle style = new MySqlStyle();
-//		OracleStyle style = new OracleStyle();
+//		MySqlStyle style = new MySqlStyle();
+		OracleStyle style = new OracleStyle();
 		
 		MySqlConnectoinSource cs = new MySqlConnectoinSource();
 		SQLLoader loader = new ClasspathLoader("/org/beetl/sql/test");
 		
 		Interceptor[] inters = new Interceptor[]{ new DebugInterceptor()};
 		SQLManager 	sql = new SQLManager(style,loader,cs,new UnderlinedNameConversion(), inters);
-		User user = new User();
-		user.setId(1);
-		sql.select("user.queryUsers", User.class, user);
+		List<LijzTest> list = sql.all(LijzTest.class);
+		System.out.println(list.get(0).getCreateTime());
 	
 	
 	}
