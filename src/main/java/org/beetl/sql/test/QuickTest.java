eb0cd9d43983165a2b94a7cf51ee16d590429975
李家智 <lijiazhi@coamc.com>
2017-02-28 10:25:58 +0800

    2.8.5 db2 support

--- src/main/java/org/beetl/sql/test/QuickTest.java
+++ src/main/java/org/beetl/sql/test/QuickTest.java
@@ -5,12 +5,8 @@ import org.beetl.sql.core.Interceptor;
 import org.beetl.sql.core.SQLLoader;
 import org.beetl.sql.core.SQLManager;
 import org.beetl.sql.core.UnderlinedNameConversion;
-<<<<<<< HEAD
-import org.beetl.sql.core.db.DB2SqlStyle;
-import org.beetl.sql.core.engine.PageQuery;
-=======
 import org.beetl.sql.core.db.MySqlStyle;
->>>>>>> 462aac45587f2fa5f4feea977e9cfa0320c89620
+import org.beetl.sql.core.engine.PageQuery;
 import org.beetl.sql.ext.DebugInterceptor;
 
 /**
@@ -34,23 +30,12 @@ public class QuickTest {
 		SQLManager 	sql = new SQLManager(style,loader,cs,new UnderlinedNameConversion(), inters);
 
 		UserDao dao = sql.getMapper(UserDao.class);
-<<<<<<< HEAD
 		PageQuery query = new  PageQuery();
 		query.setPageNumber(2);
 		query.setPageSize(2);
 		query.setOrderBy("id desc");
 		dao.queryUsers(query);
 		int a = 1;
-=======
-//		dao.all();
-		sql.update("user.updateUser");
-		
-//		User user = dao.unique(8);
-//		System.out.println(user.getCreateTime());
-		
-//		sql.genPojoCodeToConsole("USER");
-//		sql.genSQLTemplateToConsole("USER");
->>>>>>> 462aac45587f2fa5f4feea977e9cfa0320c89620
 		
 		
 		
