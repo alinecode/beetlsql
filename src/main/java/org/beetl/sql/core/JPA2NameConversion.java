433ea3b1e72ddc221925f91a7aa4ffbbe89e6be6
linziguan <linziguan@live.com>
2017-06-22 08:59:39 +0800

    col到property是可能有对应关系的，即使property被标注了Transient

--- src/main/java/org/beetl/sql/core/JPA2NameConversion.java
+++ src/main/java/org/beetl/sql/core/JPA2NameConversion.java
@@ -8,7 +8,7 @@ public class JPA2NameConversion extends NameConversion{
 	
 	NameConversion nc = null;
 	public JPA2NameConversion(){
-		
+		nc=new DefaultNameConversion();
 	}
 	/**
 	 * 对于没有jpa注解的，采用的命名策略，包括tail的命名策略，如果nc为null，则直接返回列名
@@ -31,7 +31,14 @@ public class JPA2NameConversion extends NameConversion{
 		if(c==null||Map.class.isAssignableFrom(c)){
 			return nc!=null?nc.getPropertyName(c, colName):colName;
 		}
-		return JPAEntityHelper.getEntityTable(c,nc).getProp(colName);
+		
+		//col到property是可能有对应关系的，即使property被标注了Transient
+		String name =  JPAEntityHelper.getEntityTable(c,nc).getProp(colName);
+		if(name!=null){
+			return name;
+		}else{
+			return nc!=null?nc.getPropertyName(c, colName):colName;
+		}
 	}
 
 	@Override
