4f1816232d227633c201105f6a1d0157e66b230d
xiandafu <xiandafu@126.com>
2018-09-15 12:00:23 +0800

    test

--- src/main/java/org/beetl/sql/test/QuickTest.java
+++ src/main/java/org/beetl/sql/test/QuickTest.java
@@ -6,11 +6,11 @@ import org.beetl.sql.core.ClasspathLoader;
 import org.beetl.sql.core.ConnectionSource;
 import org.beetl.sql.core.ConnectionSourceHelper;
 import org.beetl.sql.core.Interceptor;
-import org.beetl.sql.core.Params;
 import org.beetl.sql.core.SQLLoader;
 import org.beetl.sql.core.SQLManager;
 import org.beetl.sql.core.UnderlinedNameConversion;
 import org.beetl.sql.core.db.MySqlStyle;
+import org.beetl.sql.core.engine.PageQuery;
 import org.beetl.sql.ext.DebugInterceptor;
 
 import com.zaxxer.hikari.HikariDataSource;
@@ -43,9 +43,10 @@ public class QuickTest {
 		
 		Interceptor[] inters = new Interceptor[]{ debug};
 		final SQLManager 	sql = new SQLManager(style,loader,cs,new UnderlinedNameConversion(), inters);
-//		sql.genPojoCodeToConsole("user", gen);
-		sql.execute("select * from user where id=#id,jdbc='i'#", User.class, new Params().add("id", 1).map());
-
+		UserDao dao = sql.getMapper(UserDao.class);
+		PageQuery query = new PageQuery();
+		query.setPara("name", "lijzhdd");
+		dao.templatePage(query);
 	}
 	
 	public static User unique(SQLManager sql,Object key){
