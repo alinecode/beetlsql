cfec68aa94392ac422e3a75cbd6ca9c00ebcac73
王伯勋 <wangbx@hsmap.com>
2019-08-26 14:59:26 +0800

    恢复默认配置

--- src/main/java/org/beetl/sql/test/MysqlDBConfig.java
+++ src/main/java/org/beetl/sql/test/MysqlDBConfig.java
@@ -1,15 +1,17 @@
 package org.beetl.sql.test;
 
 public class MysqlDBConfig {
-	public static String driver = "com.mysql.cj.jdbc.Driver";
-    public static String dbName = "blog";
-    public static String password = "8975789757";
+//    public static String driver = "com.mysql.jdbc.Driver";
+    public static String driver = "com.mysql.cj.jdbc.Driver";
+    public static String dbName = "orm";
+    public static String password = "123456";
     public static String userName = "root";
-    public static String url = "jdbc:mysql://mysql:3306/" + dbName+"?&serverTimezone=UTC&useSSL=false";
-    
-    
-    
-    
+    public static String url = "jdbc:mysql://127.0.0.1:3306/" + dbName+"?&serverTimezone=UTC";
+//    public static String url = "jdbc:mysql://127.0.0.1:3306/" + dbName+"?&serverTimezone=UTC&useSSL=false";
+
+
+
+
 //    public static String driver = "com.ibm.db2.jcc.DB2Driver";
 //    public static String dbName = "TESTDB";
 //    public static String password = "db2inst1_1";
@@ -21,17 +23,18 @@ public class MysqlDBConfig {
 //    public static String password = "develop";
 //    public static String userName = "dev";
 //    public static String url = "jdbc:sqlserver://175.102.10.132:1733;databaseName=dev";  ;
-    
+
 //    public static String driver = "oracle.jdbc.driver.OracleDriver";
 //    public static String dbName = "risk";
 //    public static String password = "risk";
 //    public static String userName = "risk";
 //    public static String url = "jdbc:oracle:thin:@172.16.86.56:1521:dfzb";
-    
+
 //  public static String driver = "org.postgresql.Driver";
 //  public static String dbName = "postgres";
 //  public static String password = "postgres";
 //  public static String userName = "postgres";
 //  public static String url = "jdbc:postgresql://192.168.1.68:5432/postgres";
-    
+
 }
+
