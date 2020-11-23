4a8dbb2fc17c6c449147e424e9f61bd965617e00
王立君 <wanglijun@xforceplus.com>
2020-11-23 09:59:57 +0800

    优化类型导入

--- sql-util/src/main/java/org/beetl/sql/clazz/kit/StringKit.java
+++ sql-util/src/main/java/org/beetl/sql/clazz/kit/StringKit.java
@@ -20,22 +20,23 @@ public class StringKit {
 	
 	// 首字母转小写
 	public static String toLowerCaseFirstOne(String s) {
-		if (Character.isLowerCase(s.charAt(0)))
+		if (Character.isLowerCase(s.charAt(0))) {
 			return s;
-		else
-			return (new StringBuilder())
-					.append(Character.toLowerCase(s.charAt(0)))
-					.append(s.substring(1)).toString();
+		}
+		return (new StringBuilder())
+				.append(Character.toLowerCase(s.charAt(0)))
+				.append(s.substring(1)).toString();
 	}
 
 	// 首字母转大写
 	public static String toUpperCaseFirstOne(String s) {
-		if (Character.isUpperCase(s.charAt(0)))
+		if (Character.isUpperCase(s.charAt(0))) {
 			return s;
-		else
+		} else {
 			return (new StringBuilder())
 					.append(Character.toUpperCase(s.charAt(0)))
 					.append(s.substring(1)).toString();
+		}
 	}
 
 	// 大写字母前面加上下划线并转为全小写
