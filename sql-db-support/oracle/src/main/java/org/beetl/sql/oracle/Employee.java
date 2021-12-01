fcd181543e1b2d7a1d9145caf132e6c2dceb0aad
lijiazhi <lijiazhi@pinefield-inc.com>
2021-12-01 21:18:09 +0800

    oracle test code in db support

---
+++ sql-db-support/oracle/src/main/java/org/beetl/sql/oracle/Employee.java
@@ -0,0 +1,16 @@
+package org.beetl.sql.oracle;
+
+import lombok.Data;
+import org.beetl.sql.annotation.entity.Table;
+
+import java.sql.Timestamp;
+import java.sql.Date;
+
+@Table(name="hr.employees")
+@Data
+public class Employee {
+	Integer employeeId;
+	String firstName;
+	Date hireDate;
+
+}
