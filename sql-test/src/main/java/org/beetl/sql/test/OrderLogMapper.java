e0d0228b17d314d9bba0abad289e364416000b58
lijiazhi <lijiazhi@pinefield-inc.com>
2022-03-08 22:00:55 +0800

    完善多库应用

--- sql-test/src/main/java/org/beetl/sql/test/OrderLogMapper.java
+++ sql-test/src/main/java/org/beetl/sql/test/OrderLogMapper.java
@@ -3,7 +3,7 @@ package org.beetl.sql.test;
 import org.beetl.sql.mapper.BaseMapper;
 import org.beetl.sql.mapper.annotation.SqlResource;
 
-import javax.annotation.Resource;
+
 import java.util.List;
 @SqlResource("user")
 public interface OrderLogMapper extends BaseMapper<OrderLog> {
