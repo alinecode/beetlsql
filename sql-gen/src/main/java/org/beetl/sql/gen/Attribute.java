bb264d5bc5cfbfa91e3ed0ee08f4496223d7ec54
orange <licardoooo@gmail.com>
2022-05-07 05:41:21 +0800

    修复第三个字符(即charAt(2))是大写时get/set生成错误

--- sql-gen/src/main/java/org/beetl/sql/gen/Attribute.java
+++ sql-gen/src/main/java/org/beetl/sql/gen/Attribute.java
@@ -11,23 +11,24 @@ import org.beetl.sql.clazz.kit.StringKit;
  */
 @Data
 public class Attribute {
-	/**根据命名规则得出来的列对应的属性名*/
+	/**
+	 * 根据命名规则得出来的列对应的属性名
+	 */
 	private String name;
 	private String colName;
-	/**Class名，比如Integer*/
+	/**
+	 * Class名，比如Integer
+	 */
 	private String javaType;
 	private boolean isId;
 	private boolean isAuto;
+	/**
+	 * 属性的注释
+	 */
 	private String comment;
 
-
 	public String getMethodName() {
-		if (name.length() > 2 && Character.isUpperCase(name.charAt(2))) {
-			return name;
-		}else{
-			return StringKit.toUpperCaseFirstOne(name);
-		}
+		return name.length() > 2 ? StringKit.toUpperCaseFirstOne(name) : name;
 	}
-
 
 }
