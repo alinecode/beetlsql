ae8090c9bd72ebc565939689fd18cf8bcd490ab9
lijiazhi <lijiazhi@pinefield-inc.com>
2022-08-07 22:02:00 +0800

    更新性能测试结果

--- sql-jmh/readme.md
+++ sql-jmh/readme.md
@@ -63,8 +63,47 @@ public interface BaseService {
 # 测试方法
 
  进入JMHMain，运行即可。如果你有新的测试方法，可以暂时屏蔽其他测试方法
- 
- # 最新结果(2021-11-21)
+
+# 最新结果 2022-08-07 
+
+```
+Benchmark                         Mode  Cnt     Score   Error   Units
+JMHMain.beetlsqlComplexMapping   thrpt    2   261.097          ops/ms
+JMHMain.beetlsqlExecuteJdbc      thrpt    2   545.487          ops/ms
+JMHMain.beetlsqlExecuteTemplate  thrpt    2   468.580          ops/ms
+JMHMain.beetlsqlFile             thrpt    2   475.087          ops/ms
+JMHMain.beetlsqlInsert           thrpt    2   260.108          ops/ms
+JMHMain.beetlsqlLambdaQuery      thrpt    2   296.364          ops/ms
+JMHMain.beetlsqlOne2Many         thrpt    2   132.043          ops/ms
+JMHMain.beetlsqlPageQuery        thrpt    2   230.195          ops/ms
+JMHMain.beetlsqlSelectById       thrpt    2   458.701          ops/ms
+JMHMain.jdbcExecuteJdbc          thrpt    2  1182.139          ops/ms
+JMHMain.jdbcInsert               thrpt    2   335.903          ops/ms
+JMHMain.jdbcSelectById           thrpt    2  1164.131          ops/ms
+JMHMain.jpaExecuteJdbc           thrpt    2   124.374          ops/ms
+JMHMain.jpaExecuteTemplate       thrpt    2   150.468          ops/ms
+JMHMain.jpaInsert                thrpt    2    88.970          ops/ms
+JMHMain.jpaOne2Many              thrpt    2   109.989          ops/ms
+JMHMain.jpaPageQuery             thrpt    2   133.352          ops/ms
+JMHMain.jpaSelectById            thrpt    2   359.050          ops/ms
+JMHMain.mybatisComplexMapping    thrpt    2   117.422          ops/ms
+JMHMain.mybatisExecuteTemplate   thrpt    2   251.915          ops/ms
+JMHMain.mybatisFile              thrpt    2   192.212          ops/ms
+JMHMain.mybatisInsert            thrpt    2   167.225          ops/ms
+JMHMain.mybatisLambdaQuery       thrpt    2    12.453          ops/ms
+JMHMain.mybatisPageQuery         thrpt    2    76.006          ops/ms
+JMHMain.mybatisSelectById        thrpt    2   265.810          ops/ms
+JMHMain.weedExecuteJdbc          thrpt    2   424.919          ops/ms
+JMHMain.weedExecuteTemplate      thrpt    2   464.600          ops/ms
+JMHMain.weedFile                 thrpt    2   498.603          ops/ms
+JMHMain.weedInsert               thrpt    2   248.010          ops/ms
+JMHMain.weedLambdaQuery          thrpt    2   383.933          ops/ms
+JMHMain.weedPageQuery            thrpt    2   249.315          ops/ms
+JMHMain.weedSelectById           thrpt    2   401.897          ops/ms
+
+```
+
+# (2021-11-21)
  
 有些dao并不支持一些特性，所以并未出现在下面列表，比如JDBC不支持自动翻页查询，MyBatis不支持one2Many等
 ```
--- sql-jmh/src/main/java/org/beetl/sql/jmh/JMHMain.java
+++ sql-jmh/src/main/java/org/beetl/sql/jmh/JMHMain.java
@@ -58,58 +58,58 @@ public class JMHMain {
 
 
     /*   JDBC,基准，有些方法性能飞快    */
-//    @Benchmark
-//    public void jdbcInsert() {
-//        jdbcService.addEntity();
-//    }
-//
-//    @Benchmark
-//    public void jdbcSelectById() {
-//        jdbcService.getEntity();
-//    }
-//
-//    @Benchmark
-//    public void jdbcExecuteJdbc() {
-//        jdbcService.executeJdbcSql();
-//    }
-//
-//
-//    /* mybatis */
-//    @Benchmark
-//    public void mybatisInsert() {
-//        myBatisSpringService.addEntity();
-//    }
-//
-//    @Benchmark
-//    public void mybatisSelectById() {
-//        myBatisSpringService.getEntity();
-//    }
-//
-//    @Benchmark
-//    public void mybatisLambdaQuery() {
-//        myBatisSpringService.lambdaQuery();
-//    }
-//
-//    @Benchmark
-//    public void mybatisExecuteTemplate() {
-//        myBatisSpringService.executeTemplateSql();
-//    }
-//
-//    @Benchmark
-//    public void mybatisFile() {
-//        myBatisSpringService.sqlFile();
-//    }
-//
-//    @Benchmark
-//    public void mybatisPageQuery() {
-//        myBatisSpringService.pageQuery();
-//    }
-//
-//    @Benchmark
-//    public void mybatisComplexMapping() {
-//        myBatisSpringService.complexMapping();
-//    }
-//
+    @Benchmark
+    public void jdbcInsert() {
+        jdbcService.addEntity();
+    }
+
+    @Benchmark
+    public void jdbcSelectById() {
+        jdbcService.getEntity();
+    }
+
+    @Benchmark
+    public void jdbcExecuteJdbc() {
+        jdbcService.executeJdbcSql();
+    }
+
+
+    /* mybatis */
+    @Benchmark
+    public void mybatisInsert() {
+        myBatisSpringService.addEntity();
+    }
+
+    @Benchmark
+    public void mybatisSelectById() {
+        myBatisSpringService.getEntity();
+    }
+
+    @Benchmark
+    public void mybatisLambdaQuery() {
+        myBatisSpringService.lambdaQuery();
+    }
+
+    @Benchmark
+    public void mybatisExecuteTemplate() {
+        myBatisSpringService.executeTemplateSql();
+    }
+
+    @Benchmark
+    public void mybatisFile() {
+        myBatisSpringService.sqlFile();
+    }
+
+    @Benchmark
+    public void mybatisPageQuery() {
+        myBatisSpringService.pageQuery();
+    }
+
+    @Benchmark
+    public void mybatisComplexMapping() {
+        myBatisSpringService.complexMapping();
+    }
+
 
     /*   BeetlSQL    */
     @Benchmark
@@ -160,72 +160,72 @@ public class JMHMain {
 
 
 //    /*   Spring Data JPA    */
-//    @Benchmark
-//    public void jpaInsert() {
-//        springService.addEntity();
-//    }
-//
-//    @Benchmark
-//    public void jpaSelectById() {
-//        springService.getEntity();
-//    }
-//
-//    @Benchmark
-//    public void jpaExecuteJdbc() {
-//        springService.executeJdbcSql();
-//    }
-//
-//    /*实际上JPA并不支持template，但勉强用HQl来测试*/
-//    @Benchmark
-//    public void jpaExecuteTemplate() {
-//        springService.executeTemplateSql();
-//    }
-//
-//    @Benchmark
-//    public void jpaOne2Many() {
-//        springService.one2Many();
-//    }
-//
-//    @Benchmark
-//    public void jpaPageQuery() {
-//        springService.pageQuery();
-//    }
-//
-//    /*   Weed3    */
-//    @Benchmark
-//    public void weedInsert() {
-//        weedService.addEntity();
-//    }
-//
-//    @Benchmark
-//    public void weedSelectById() {
-//        weedService.getEntity();
-//    }
-//
-//    @Benchmark
-//    public void weedLambdaQuery() {
-//        weedService.lambdaQuery();
-//    }
-//
-//    @Benchmark
-//    public void weedExecuteJdbc() {
-//        weedService.executeJdbcSql();
-//    }
-//
-//    @Benchmark
-//    public void weedExecuteTemplate() {
-//        weedService.executeTemplateSql();
-//    }
-//
-//    @Benchmark
-//    public void weedFile() {
-//        weedService.sqlFile();
-//    }
-//
-//    @Benchmark
-//    public void weedPageQuery() {
-//        weedService.pageQuery();
-//    }
+    @Benchmark
+    public void jpaInsert() {
+        springService.addEntity();
+    }
+
+    @Benchmark
+    public void jpaSelectById() {
+        springService.getEntity();
+    }
+
+    @Benchmark
+    public void jpaExecuteJdbc() {
+        springService.executeJdbcSql();
+    }
+
+    /*实际上JPA并不支持template，但勉强用HQl来测试*/
+    @Benchmark
+    public void jpaExecuteTemplate() {
+        springService.executeTemplateSql();
+    }
+
+    @Benchmark
+    public void jpaOne2Many() {
+        springService.one2Many();
+    }
+
+    @Benchmark
+    public void jpaPageQuery() {
+        springService.pageQuery();
+    }
+
+    /*   Weed3    */
+    @Benchmark
+    public void weedInsert() {
+        weedService.addEntity();
+    }
+
+    @Benchmark
+    public void weedSelectById() {
+        weedService.getEntity();
+    }
+
+    @Benchmark
+    public void weedLambdaQuery() {
+        weedService.lambdaQuery();
+    }
+
+    @Benchmark
+    public void weedExecuteJdbc() {
+        weedService.executeJdbcSql();
+    }
+
+    @Benchmark
+    public void weedExecuteTemplate() {
+        weedService.executeTemplateSql();
+    }
+
+    @Benchmark
+    public void weedFile() {
+        weedService.sqlFile();
+    }
+
+    @Benchmark
+    public void weedPageQuery() {
+        weedService.pageQuery();
+    }
 
 
     public static void main(String[] args) throws RunnerException {
