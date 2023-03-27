d229b8a2a1624188a409260ccbaf70487a96d269
李家智 <lijiazhi@haier.com>
2023-03-27 18:48:44 +0800

    bug fix since 阳了后

---
+++ sql-xml/src/main/java/org/beetl/sql/xml/XMLLoader.java
@@ -0,0 +1,49 @@
+package org.beetl.sql.xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class XMLLoader {
+	public static void main(String[] args) throws Exception{
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+			//创建DocumentBuilder对象
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			db.setErrorHandler(new ErrorHandler() {
+			@Override
+			public void warning(SAXParseException e) throws SAXException {
+				System.out.println(e);
+			}
+
+			@Override
+			public void error(SAXParseException e) throws SAXException {
+				System.out.println(e);
+			}
+
+			@Override
+			public void fatalError(SAXParseException e) throws SAXException {
+				System.out.println(e);
+			}
+		});
+			//通过DocumentBuilder对象的parser方法加载books.xml文件到当前项目下
+			Document document = db.parse(XMLLoader.class.getResourceAsStream("/sql/user.xml"));
+
+			//获取所有book节点的集合
+			NodeList sqlList = document.getElementsByTagName("sql");
+			int len = sqlList.getLength();
+			for(int i=0;i<len;i++){
+				Node node = sqlList.item(i);
+				node.get
+				System.out.println(node.getTextContent());
+			}
+
+
+	}
+}
