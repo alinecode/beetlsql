36559bf8f7574d71d3d3b42d7b2ba8c682b2b648
lijiazhi <lijiazhi@pinefield-inc.com>
2023-07-04 03:49:43 +0800

    oralce12 support

--- sql-db-support/oracle/src/main/java/org/beetl/sql/oracle/Employee.java
+++ sql-db-support/oracle/src/main/java/org/beetl/sql/oracle/Employee.java
@@ -1,16 +1,20 @@
 package org.beetl.sql.oracle;
 
 import lombok.Data;
+import org.beetl.sql.annotation.entity.Column;
 import org.beetl.sql.annotation.entity.Table;
 
 import java.sql.Timestamp;
 import java.util.Date;
 
-@Table(name="hr.employees")
+@Table(name="scott.emp")
 @Data
 public class Employee {
-	Integer employeeId;
-	String firstName;
-	Date hireDate;
+
+	@Column("empno")
+	Integer empno;
+	@Column("ENAME")
+	String ename;
+
 
 }
--- sql-db-support/oracle/src/main/java/org/beetl/sql/oracle/OracleTest.java
+++ sql-db-support/oracle/src/main/java/org/beetl/sql/oracle/OracleTest.java
@@ -3,9 +3,11 @@ package org.beetl.sql.oracle;
 import com.zaxxer.hikari.HikariDataSource;
 import org.beetl.sql.core.*;
 import org.beetl.sql.core.db.MySqlStyle;
+import org.beetl.sql.core.db.Oracle12Style;
 import org.beetl.sql.core.db.OracleStyle;
 import org.beetl.sql.core.page.DefaultPageRequest;
 import org.beetl.sql.core.page.PageRequest;
+import org.beetl.sql.core.page.PageResult;
 import org.beetl.sql.ext.DebugInterceptor;
 import org.beetl.sql.oracle.Employee;
 
@@ -26,21 +28,29 @@ public class OracleTest {
         SQLManagerBuilder builder = new SQLManagerBuilder(source);
         builder.setNc(new UnderlinedNameConversion());
         builder.setInters(new Interceptor[]{new DebugInterceptor()});
-        builder.setDbStyle(new OracleStyle());
+        builder.setDbStyle(new Oracle12Style());
         SQLManager sqlManager = builder.build();
 
 
-		long time = System.currentTimeMillis();
-		Date start = new Date(time-100);
-		Date end = new Date(time+100);
-		long count = sqlManager.lambdaQuery(Employee.class).andBetween(Employee::getHireDate,start,end).count();
+		{
+			String sql = "SELECT e.* FROM scott.EMP e  ";
+			PageRequest pageRequest = DefaultPageRequest.of(1,5);
+			PageResult<Employee> list = sqlManager.execute(new SQLReady(sql),Employee.class,pageRequest);
+		}
+
+		{
+			String sql = "SELECT #{page()} FROM scott.EMP e  ";
+			PageRequest pageRequest = DefaultPageRequest.of(1,10);
+			PageResult<Employee> list = sqlManager.executePageQuery(sql,Employee.class,null,pageRequest);
+		}
+
 
 
     }
 
     public static DataSource datasource() {
         HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl("jdbc:oracle:thin:@127.0.0.1:49161:xe");
+        ds.setJdbcUrl("jdbc:oracle:thin:@127.0.0.1:1521:xe");
         ds.setUsername("system");
         ds.setPassword("oracle");
         ds.setDriverClassName("oracle.jdbc.OracleDriver");
