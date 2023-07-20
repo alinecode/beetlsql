7809fcc37b353fcadd1541b40341d595e9790ef2
noear <noear@live.cn>
2023-07-20 19:56:02 +0800

    sql-solon-plugin 升级到 solon 2.4.0

--- sql-integration/sql-solon-plugin/src/test/java/demo/Config.java
+++ sql-integration/sql-solon-plugin/src/test/java/demo/Config.java
@@ -1,6 +1,11 @@
 package demo;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.beetl.sql.core.DefaultNameConversion;
+import org.beetl.sql.core.SQLManager;
+import org.beetl.sql.core.db.MySqlStyle;
+import org.beetl.sql.solon.annotation.Db;
+
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
@@ -9,8 +14,20 @@ import javax.sql.DataSource;
 
 @Configuration
 public class Config {
-    @Bean(value = "db1")
-    public DataSource sqlSessionFactory1(@Inject("${test.db1}") HikariDataSource dataSource) throws Exception{
+	/**
+	 * 构建数据源
+	 * */
+    @Bean("db1")
+    public DataSource db1(@Inject("${test.db1}") HikariDataSource dataSource) throws Exception{
        return dataSource;
     }
+
+	/**
+	 * 如果有需要，对 SQLManager 进行微调（一般，使用 yml 配置）
+	 * */
+	@Bean
+	public void db1Init(@Db("db1")SQLManager sqlManager){
+		sqlManager.setDbStyle(new MySqlStyle());
+		sqlManager.setNc(new DefaultNameConversion());
+	}
 }
