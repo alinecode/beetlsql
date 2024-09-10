922032882e888f1ad03d91fb85fa5bad4f3a6734
noear <noear@live.cn>
2024-09-10 17:01:46 +0800

    solon 升为 2.9.3；更换弃用接口

--- sql-integration/sql-solon-plugin/src/test/java/features/dynamic/DynamicTest.java
+++ sql-integration/sql-solon-plugin/src/test/java/features/dynamic/DynamicTest.java
@@ -1,12 +1,9 @@
 package features.dynamic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.noear.solon.annotation.Inject;
-import org.noear.solon.test.SolonJUnit4ClassRunner;
 import org.noear.solon.test.SolonTest;
 
-@RunWith(SolonJUnit4ClassRunner.class)
 @SolonTest(DynamicApp.class)
 public class DynamicTest {
     @Inject
